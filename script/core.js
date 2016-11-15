de29a667b312eb80a4340d8531ecb0647254901f
MeDVen <darkvenomx@gmail.com>
2016-11-15 18:41:04 +0300

    Adding theme choice. First step.

--- script/core.js
+++ script/core.js
@@ -182,5 +182,21 @@ function fillValueField(value) {
     }
 }
 
+/**
+ * Change CSS file on the fly. For the full change^ index must be same as a connected css file in header tag.
+ * @param cssFile new css file
+ * @param cssLinkIndex css link index from header
+ */
+function changeCSS(cssFile, cssLinkIndex) {
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
 initOperations();
 initNumberButtonsActions();
